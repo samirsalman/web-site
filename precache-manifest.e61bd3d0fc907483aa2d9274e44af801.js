6e4860e069691d209088cca9fca33e595c5a279d
samirsalman <33979978+samirsalman@users.noreply.github.com>
2019-12-15 12:16:14 +0100

    Updates

---
+++ precache-manifest.e61bd3d0fc907483aa2d9274e44af801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09937030fbdc2c45493f6c29b5075b3c",
+    "url": "/web-site/index.html"
+  },
+  {
+    "revision": "f8927d98689f2c2888de",
+    "url": "/web-site/static/css/2.bead03cf.chunk.css"
+  },
+  {
+    "revision": "e118717984184f5457aa",
+    "url": "/web-site/static/css/main.802392dc.chunk.css"
+  },
+  {
+    "revision": "f8927d98689f2c2888de",
+    "url": "/web-site/static/js/2.bd7efcae.chunk.js"
+  },
+  {
+    "revision": "bc5c8fb1f55404f707cd9ceb538874de",
+    "url": "/web-site/static/js/2.bd7efcae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e118717984184f5457aa",
+    "url": "/web-site/static/js/main.81c91177.chunk.js"
+  },
+  {
+    "revision": "8bef0cbe70db55ad7813",
+    "url": "/web-site/static/js/runtime-main.44e8e30d.js"
+  }
+]);

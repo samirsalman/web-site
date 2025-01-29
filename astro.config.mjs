f68188636ac7bfeb9b7a8eb60ad0ca8689daa5bb
samir <samir.salman@stip.io>
2025-01-29 16:28:50 +0100

    trial

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://samirsalman.github.io',
-	base: "web-site",
 	integrations: [mdx(), sitemap()],
 });
